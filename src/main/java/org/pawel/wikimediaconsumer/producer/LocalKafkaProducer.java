c87fa80563dee732ded0670f111f949d86c39678
pawso <sokerowsky@gmail.com>
2024-05-07 10:21:59 +0200

    Fixed lazy initialization

--- src/main/java/org/pawel/wikimediaconsumer/producer/LocalKafkaProducer.java
+++ src/main/java/org/pawel/wikimediaconsumer/producer/LocalKafkaProducer.java
@@ -2,15 +2,9 @@ package org.pawel.wikimediaconsumer.producer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.concurrent.ConcurrentException;
-import org.apache.commons.lang3.concurrent.ConcurrentInitializer;
-import org.apache.commons.lang3.concurrent.LazyInitializer;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.pawel.wikimediaconsumer.config.KafkaProducerConfig;
 import org.springframework.stereotype.Component;
-
-import java.util.Properties;
 
 @Slf4j
 @Component
@@ -18,45 +12,12 @@ import java.util.Properties;
 public class LocalKafkaProducer {
 
     final KafkaProducerConfig kafkaProducerConfig;
+    final LazyKafkaProducerProvider producerProvider;
 
     public void consume(String key, String message) {
-        KafkaProducer producer;
-        try {
-            producer = createOrGetProducer();
-        } catch (ConcurrentException e) {
-            log.error("Could not create kafka producer", e);
-            throw new RuntimeException(e);
-        }
 
+        var producer = producerProvider.get();
         ProducerRecord<String, String> record = new ProducerRecord<>(kafkaProducerConfig.getTopic(), message);
-
         producer.send(record);
     }
-
-    private KafkaProducer<String, String> createOrGetProducer() throws ConcurrentException {
-        var props = createProperties();
-
-        ConcurrentInitializer<KafkaProducer<String, String>> lazyInitializer = new LazyInitializer<>() {
-            @Override
-            protected KafkaProducer<String, String> initialize()  {
-                return new KafkaProducer<>(props);
-            }
-        };
-
-        return lazyInitializer.get();
-    }
-
-    private Properties createProperties() {
-        Properties properties = new Properties();
-
-        properties.put("bootstrap.servers", kafkaProducerConfig.getBootstrapServers());
-        properties.put("sasl.mechanism", kafkaProducerConfig.getSaslMechanism());
-        properties.put("security.protocol", kafkaProducerConfig.getSecurityProtocol());
-        properties.put("sasl.jaas.config", kafkaProducerConfig.getSaslJaasConfig());
-        properties.put("key.serializer", kafkaProducerConfig.getKeySerializer());
-        properties.put("value.serializer", kafkaProducerConfig.getValueSerializer());
-
-        return properties;
-    }
-
 }
